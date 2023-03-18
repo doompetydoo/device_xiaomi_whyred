00aa27782d1d85024ed23f70c7d8bdc84d593a1e
HeroBuxx <me@herobuxx.me>
2023-03-18 12:24:42 +0000

    whyred: It's Halcyon time
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_whyred.mk
+    $(LOCAL_DIR)/halcyon_whyred.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_whyred-user \
-    lineage_whyred-userdebug \
-    lineage_whyred-eng
+    halcyon_whyred-user \
+    halcyon_whyred-userdebug \
+    halcyon_whyred-eng
